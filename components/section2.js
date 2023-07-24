5483efd6edccea94aa53d885db47ab00f07a2e34
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-07-24 10:01:35 +0100

    added dynamic data to posts section

--- components/section2.js
+++ components/section2.js
@@ -1,32 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
 import Author from "./_child/author";
+import fetcher from "../lib/fetcher";
 
 export default function section2() {
+  const { data, isLoading, isError } = fetcher("api/posts");
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
   return (
     <section className="container mx-auto md:px-20 py-10">
       <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
 
       {/* grid columns */}
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+        {data.map((value, index) => (
+          <Post data={value} key={index}></Post>
+        ))}
       </div>
     </section>
   );
 }
 
-function Post() {
+function Post({ data }) {
+  const { id, title, subtitle, category, img, published, author } = data;
+
   return (
     <div className="item">
       <div className="images">
         <Link href={"/"}>
           <Image
-            src={"/images/img1.jpg"}
+            alt="post"
+            src={img || "/"}
             className="rounded"
             width={500}
             height={350}
@@ -37,27 +42,24 @@ function Post() {
         <div className="cat">
           <Link href={"/"} legacyBehavior>
             <a className="text-orange-600 hover:text-orange-800">
-              Business, Travel
+              {category || "Unknown"}
             </a>
           </Link>
           <Link href={"/"} legacyBehavior>
-            <a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a>
+            <a className="text-gray-800 hover:text-gray-600">
+              {published || "Unknown"}
+            </a>
           </Link>
         </div>
         <div className="title">
           <Link href={"/"} legacyBehavior>
             <a className="text-xl font-bold text-gray-800 hover:text-gray-600">
-              Your most unhappy customers are your greatest source of learning
+              {title || "Title"}
             </a>
           </Link>
         </div>
-        <p className="text-gray-500 py-3">
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar.
-        </p>
-        <Author></Author>
+        <p className="text-gray-500 py-3">{subtitle || "Subtitle"}</p>
+        {author ? <Author></Author> : <></>}
       </div>
     </div>
   );
