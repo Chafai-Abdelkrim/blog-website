1e7099f99441dff6f3008f2e82e47b4cfccc04e9
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-07-25 10:02:51 +0100

    added dynamic routes

---
+++ pages/posts/[postId].js
@@ -0,0 +1,61 @@
+import Format from "../../layout/format";
+import Author from "../../components/_child/author";
+import Image from "next/image";
+import Related from "../../components/_child/related";
+import getPost from "@/lib/helper";
+
+export default function Page({ title, img, subtitle, description, author }) {
+  return (
+    <Format>
+      <section className="container mx-auto md:px-2 py-16 w-1/2">
+        <div className="flex justify-center">
+          {author ? <Author></Author> : <></>}
+        </div>
+
+        <div className="post py-10">
+          <h1 className="font-bold text-4xl text-center pb-5">
+            {title || "No title"}
+          </h1>
+
+          <p className="text-gray-500 text-xl text-center">
+            {subtitle || "No subtitle"}
+          </p>
+
+          <div className="py-10">
+            <Image src={img || "/"} width={900} height={600}></Image>
+          </div>
+
+          <div className="content text-gray-600 text-lg flex flex-col gap-4">
+            {description || "No description"}
+          </div>
+        </div>
+        <Related></Related>
+      </section>
+    </Format>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const posts = await getPost(params.postId);
+
+  return {
+    props: posts,
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPost();
+
+  const paths = posts.map((value) => {
+    return {
+      params: {
+        postId: value.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
